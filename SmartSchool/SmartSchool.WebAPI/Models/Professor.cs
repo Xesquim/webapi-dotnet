d7353bd00564c34ee68990bce54cb077c13ec507
Xesquim <matheus.g.cesquim@gmail.com>
2023-08-04 15:07:45 -0300

    Adding controllers and models

---
+++ SmartSchool/SmartSchool.WebAPI/Models/Professor.cs
@@ -0,0 +1,17 @@
+﻿namespace SmartSchool.WebAPI.Models
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int DisciplinaId { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
+        public Professor() { }
+        public Professor(int id, string nome, int disciplinaId)
+        {
+            Id = id;
+            Nome = nome;
+            DisciplinaId = disciplinaId;
+        }
+    }
+}
